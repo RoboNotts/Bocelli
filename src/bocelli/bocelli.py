077d1557f2c4753887fdd4286bfae676f0371d64
psyav2 <psyav2@nottingham.ac.uk>
2022-10-07 14:50:15 +0000

    Initial Attempt at Verbal Node

---
+++ src/bocelli/bocelli.py
@@ -0,0 +1,28 @@
+from urllib import robotparser
+import rospy
+from speech.SpeechText import Speaker
+from bocelli.msg import ListenResult, SpeechRequest, ListenRequest
+
+class Bocelli:
+    def __init__(self):
+        self.speaker = Speaker()
+
+        self.publishers = {
+            "listen_result": rospy.Publisher('bocelli/hear', ListenResult,queue_size=10)
+        }
+        self.subscribers = {
+            "speak_request": rospy.Subscriber('bocelli/speak', SpeechRequest, self._onSpeechRequest),
+            "listen_request": rospy.Subscriber('bocelli/requestListen', ListenRequest, self._onListenRequest)
+        }
+    
+    def _onSpeechRequest(self, msg):
+        self.speaker.Speak(msg)
+    
+    def _onListenRequest(self, msg):
+        result = self.speaker.Listen(msg.duration)
+        self.publishers["listen_result"].publish(result)
+
+    def main(*args, **kwargs):
+        rospy.init_node('bocelli')
+        d = Bocelli()
+        rospy.spin()
---
+++ src/speech/__init__.py
@@ -0,0 +1,1 @@
+from speech.SpeechText import Speaker
