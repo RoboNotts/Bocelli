3233463b2bbd4c2957df3b0b3bbba1538105eaab
Adrian Vecina Tercero <avecinat@gmail.com>
2022-10-12 16:35:00 +0100

    ROB-10 Added simple comments

--- src/speech/SpeechText.py
+++ src/speech/SpeechText.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import pyttsx3
 
+# Provides speaking and listening services
 class Speaker:
     
     def __init__(self):
@@ -9,20 +10,23 @@ class Speaker:
         voices = self.engine.getProperty("voices") 
         self.engine.setProperty("voice", voices[11].id) 
 
+    # Uses text-to-speech to say something
     def Speak(self, command):
         self.engine.say(command)
         self.engine.runAndWait()
         
+    # Listens for a set amount of time, and returns what was said to it.
     def Listen(self, dur):
         with sr.Microphone() as source:
             self.Speak("Please give me a moment.")
             self.r.adjust_for_ambient_noise(source, duration=dur)
-            self.Speak("Okay, tell me what you're doing please.")
+            self.Speak("Okay, tell me what you're doing please.") # Temporary addition for METRICS
             print("listening") #prints
             audio = self.r.listen(source)
             text = self.r.recognize_google(audio)
         return text
 
+# Testing
 if __name__ == "__main__":
     try:
         test = Speaker()
