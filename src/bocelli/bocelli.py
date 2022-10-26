e3dad315396afea8c80ca63ebc945a655828c7b0
Adrian Vecina Tercero <psyav2@nottingham.ac.uk>
2022-10-26 14:35:32 +0100

    Changed Bocelli to be serviced-based

--- src/bocelli/bocelli.py
+++ src/bocelli/bocelli.py
@@ -1,29 +1,27 @@
 from urllib import robotparser
 import rospy
 from speech.SpeechText import Speaker
-from bocelli.msg import ListenResult, SpeechRequest, ListenRequest
+from bocelli.srv import Listen, Speak, SpeakResponse, ListenResponse
 
 # ROS node to interface with speaking/listening
 class Bocelli:
     def __init__(self):
         self.speaker = Speaker()
 
-        self.publishers = {
-            "listen_result": rospy.Publisher('bocelli/hear', ListenResult,queue_size=10)
-        }
-        self.subscribers = {
-            "speak_request": rospy.Subscriber('bocelli/speak', SpeechRequest, self._onSpeechRequest),
-            "listen_request": rospy.Subscriber('bocelli/requestListen', ListenRequest, self._onListenRequest)
+        self.services = {
+            "speak_request": rospy.Service('speak', Speak, self._onSpeechRequest),
+            "listen_request":rospy.Service('listen', Listen, self._onListenRequest)
         }
     
     def _onSpeechRequest(self, msg):
-        self.speaker.Speak(msg.result)
+        self.speaker.Speak(msg.request)
+        return SpeakResponse(1)
     
     def _onListenRequest(self, msg):
         result = self.speaker.Listen(msg.duration)
-        out = ListenResult()
+        out = ListenResponse()
         out.result = result
-        self.publishers["listen_result"].publish(out)
+        return out
 
     def main(*args, **kwargs):
         rospy.init_node('bocelli')
